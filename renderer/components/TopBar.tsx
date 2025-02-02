9cfe4b38f0a0c5d43afc08bfb7012199c22a3558
Lakmina Egodawattha <lakmina@bistecglobal.com>
2025-02-02 11:34:24 +0530

    fixeeee

--- renderer/components/TopBar.tsx
+++ renderer/components/TopBar.tsx
@@ -44,7 +44,9 @@ const TopBar = () => {
     <div className="bg-white shadow-sm">
       <div className="h-16 flex items-center justify-between px-4">
         <div className="flex-1">
-          <h1 className="text-2xl font-semibold text-gray-800">Point of</h1>
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Point of Sale
+          </h1>
         </div>
 
         {/* User Menu */}
